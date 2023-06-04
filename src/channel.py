d1fb2b39948fd8b6c5debee5541f351f8b5d4f0e
m.sumbaev <sumbaevmaksim98@gmail.com>
2023-06-04 11:38:07 +0300

    changed channel.py

--- src/channel.py
+++ src/channel.py
@@ -13,11 +13,13 @@ def printj(dict_to_print: dict) -> None:
     print(json.dumps(dict_to_print, indent=2, ensure_ascii=False))
 
 
-youtube = build('youtube', 'v3', developerKey=api_key)
+# youtube = build('youtube', 'v3', developerKey=api_key)
 
 
 class Channel:
+
     """Класс для ютуб-канала"""
+    youtube = build('youtube', 'v3', developerKey=api_key)
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
@@ -34,7 +36,7 @@ class Channel:
         return self.__channel_id
 
     def get_info(self):
-        return youtube.channels().list(id=self.__channel_id, part='snippet,statistics').execute()
+        return self.youtube.channels().list(id=self.__channel_id, part='snippet,statistics').execute()
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
@@ -44,7 +46,7 @@ class Channel:
     @classmethod
     def get_service(cls):
         """Возвращает объект для работы с YouTube API"""
-        return build('youtube', 'v3', developerKey=api_key)
+        return cls.youtube
 
     def to_json(self, channel_name: str):
         """Сохраняет в data_channels/channel_name.json значения атрибутов экземпляра 'Channel'

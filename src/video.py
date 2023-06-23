96d5c998c47f78558d771751cf9e590f5d32bb68
m.sumbaev <sumbaevmaksim98@gmail.com>
2023-06-23 21:30:08 +0300

    changed video.py

--- src/video.py
+++ src/video.py
@@ -16,10 +16,18 @@ class Video:
     def __init__(self, video_id: str) -> None:
         """Экземпляр инициализируется id видео. Дальше все данные будут подтягиваться по API."""
         self.__video_id = video_id
-        self.title = self.video_response()['items'][0]['snippet']['title']
-        self.url = f"https://www.youtube.com/watch?v={self.__video_id}"
-        self.view_count = self.video_response()['items'][0]['statistics']['viewCount']
-        self.like_count = self.video_response()['items'][0]['statistics']['likeCount']
+        try:
+            self.title = self.video_response()['items'][0]
+        except IndexError:
+            self.title = None
+            self.url = None
+            self.view_count = None
+            self.like_count = None
+        else:
+            self.title = self.video_response()['items'][0]['snippet']['title']
+            self.url = f"https://www.youtube.com/watch?v={self.__video_id}"
+            self.view_count = self.video_response()['items'][0]['statistics']['viewCount']
+            self.like_count = self.video_response()['items'][0]['statistics']['likeCount']
 
     def __str__(self):
         return f'{self.title}'
